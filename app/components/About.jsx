e8b96ed077a62a6dce2e0d9bac3189794d987229
Esso <marc.esso01@gmail.com>
2017-09-02 17:12:03 +0300

    Editing the nav bar

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,7 @@ var About = React.createClass({
         return(
             <div>
                 <h3 className= "AddEmployeeTitle"> About the App</h3>
-                <p>This applicaion is an Employee Directory App. It was done as a challenge, in order to familiarize myself with NodeJS, ReactJS, JSX Syntax, Webpack, SCSS and so many tools and frameworks.</p>
+                <p>This application is an Employee Directory App. It was done as a challenge, in order to familiarize myself with NodeJS, ReactJS, JSX Syntax, Webpack, SCSS and so many tools and frameworks.</p>
             </div>
         );
     }
